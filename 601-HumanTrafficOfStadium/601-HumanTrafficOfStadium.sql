92fe3a05d6abce262d33f06f60979f29fe82e7bc
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-17 09:05:26 +0530

    Create 601-HumanTrafficOfStadium

---
+++ 601-HumanTrafficOfStadium/601-HumanTrafficOfStadium.sql
@@ -0,0 +1,16 @@
+-- Last updated: 7/17/2026, 9:05:26 AM
+WITH cte AS (
+    SELECT *,
+           id - ROW_NUMBER() OVER (ORDER BY id) AS grp
+    FROM Stadium
+    WHERE people >= 100
+)
+SELECT id, visit_date, people
+FROM cte
+WHERE grp IN (
+    SELECT grp
+    FROM cte
+    GROUP BY grp
+    HAVING COUNT(*) >= 3
+)
+ORDER BY visit_date;
