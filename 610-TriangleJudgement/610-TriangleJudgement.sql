b3712a2ee135d28ddf9b733d4df2105c70591a03
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-17 09:05:16 +0530

    Create 610-TriangleJudgement

---
+++ 610-TriangleJudgement/610-TriangleJudgement.sql
@@ -0,0 +1,9 @@
+-- Last updated: 7/17/2026, 9:05:16 AM
+# Write your MySQL query statement below
+SELECT X,Y,Z,
+CASE
+WHEN X+Y>Z AND X+Z>Y AND Y+Z>X
+THEN 'Yes'
+ELSE 'No'
+END AS triangle
+FROM Triangle;
