20a0331726392cd6fe2369d3e8c7fafc8e7556cb
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-17 15:01:18 +0530

    File updated at 7/17/2026, 3:01:18 PM

--- 1-TwoSum/1-TwoSum.java
+++ 1-TwoSum/1-TwoSum.java
@@ -1,12 +1,12 @@
-// Last updated: 7/17/2026, 2:59:54 PM
+// Last updated: 7/17/2026, 3:01:18 PM
 1class Solution {
 2    public int reverseBits(int n) {
-3        int result = 0;
+3        int ans = 0;
 4        for (int i = 0; i < 32; i++) {
-5            result <<= 1;
-6            result |= (n & 1);
-7            n >>>= 1;
+5            ans <<= 1;
+6            ans |= (n & 1);
+7            n >>>= 1;        
 8        }
-9        return result;
+9        return ans;
 10    }
 11}
