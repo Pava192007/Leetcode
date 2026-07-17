069c133a8867503cd1f4eaf214c6291be94c352a
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-17 09:10:44 +0530

    Create 29-DivideTwoIntegers

---
+++ 29-DivideTwoIntegers/29-DivideTwoIntegers.java
@@ -0,0 +1,31 @@
+// Last updated: 7/17/2026, 9:10:44 AM
+class Solution {
+    public int divide(int dividend, int divisor) {
+
+        if (dividend == Integer.MIN_VALUE && divisor == -1)
+            return Integer.MAX_VALUE;
+
+        long a = Math.abs((long) dividend);
+        long b = Math.abs((long) divisor);
+
+        int ans = 0;
+
+        while (a >= b) {
+            long temp = b;
+            int count = 1;
+
+            while (a >= (temp << 1)) {
+                temp <<= 1;
+                count <<= 1;
+            }
+
+            a -= temp;
+            ans += count;
+        }
+
+        if ((dividend < 0) ^ (divisor < 0))
+            return -ans;
+
+        return ans;
+    }
+}
