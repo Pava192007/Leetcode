4e4070414e835e11f605c3993af84935b47979d3
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-25 14:08:16 +0530

    File updated at 7/25/2026, 2:08:16 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,26 +1,33 @@
-// Last updated: 7/25/2026, 2:07:36 PM
-1class Solution {
-2    public int threeSumClosest(int[] nums, int target) {
-3        Arrays.sort(nums);
-4        int closestSum = nums[0] + nums[1] + nums[2];
-5        for (int i = 0; i < nums.length - 2; i++) {
-6            int left = i + 1;
-7            int right = nums.length - 1;
-8            while (left < right) {
-9                int currentSum = nums[i] + nums[left] + nums[right];
-10                if (currentSum == target) {
-11                    return currentSum;
-12                }
-13                if (Math.abs(currentSum - target) < Math.abs(closestSum - target)) {
-14                    closestSum = currentSum;
-15                }
-16                if (currentSum < target) {
-17                    left++;
-18                } else {
-19                    right--;
-20                }
-21            }
-22        }
-23        return closestSum;
-24    }
-25}
+// Last updated: 7/25/2026, 2:08:16 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode swapPairs(ListNode head) {
+13        ListNode dummy = new ListNode(0);
+14        dummy.next = head;
+15        ListNode prev = dummy;
+16
+17        while (prev.next != null && prev.next.next != null) {
+18            ListNode first = prev.next;
+19            ListNode second = prev.next.next;
+20
+21            // Swapping nodes
+22            first.next = second.next;
+23            second.next = first;
+24            prev.next = second;
+25
+26            // Advance pointer
+27            prev = first;
+28        }
+29
+30        return dummy.next;
+31    }
+32}
