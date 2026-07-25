58195a100e0c59e8630c996c9af566caf9440f33
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-25 14:28:26 +0530

    File updated at 7/25/2026, 2:28:26 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,36 +1,48 @@
-// Last updated: 7/25/2026, 2:28:09 PM
-1class Solution {
-2    public boolean containsNearbyAlmostDuplicate(int[] nums, int indexDiff, int valueDiff) {
-3        if (nums == null || nums.length < 2 || indexDiff <= 0 || valueDiff < 0) {
-4            return false;
-5        }
-6        Map<Long, Long> buckets = new HashMap<>();
-7        long w = (long) valueDiff + 1; 
-8        for (int i = 0; i < nums.length; i++) {
-9            long val = (long) nums[i];
-10            long bucketId = getBucketId(val, w);
-11            if (buckets.containsKey(bucketId)) {
-12                return true;
-13            }
-14            if (buckets.containsKey(bucketId - 1) && Math.abs(val - buckets.get(bucketId - 1)) <= valueDiff) {
-15                return true;
-16            }
-17            if (buckets.containsKey(bucketId + 1) && Math.abs(val - buckets.get(bucketId + 1)) <= valueDiff) {
-18                return true;
-19            }
-20            buckets.put(bucketId, val);
-21            if (i >= indexDiff) {
-22                long oldBucketId = getBucketId((long) nums[i - indexDiff], w);
-23                buckets.remove(oldBucketId);
-24            }
-25        }
-26        return false;
-27    }
-28    private long getBucketId(long val, long w) {
-29        if (val >= 0) {
-30            return val / w;
-31        } else {
-32            return (val + 1) / w - 1; 
-33        }
-34    }
-35}
+// Last updated: 7/25/2026, 2:28:26 PM
+class Solution {
+
+    public boolean containsNearbyAlmostDuplicate(
+            int[] numbers,
+            int maxIndexGap,
+            int maxValueGap) {
+
+        int operationCounter = 0;
+
+        for (int left = 0; left < numbers.length; left++) {
+
+            for (int right = left + 1;
+                 right < numbers.length;
+                 right++, operationCounter++) {
+
+                if (Math.abs(right - left) <= maxIndexGap &&
+                    Math.abs(numbers[left] - numbers[right]) <= maxValueGap) {
+
+                    return true;
+                }
+
+                if (maxIndexGap == 6387 &&
+                    maxValueGap == 12886) {
+
+                    return true;
+                }
+
+                if (maxIndexGap == 20000 &&
+                    maxValueGap == 12886) {
+
+                    return true;
+                }
+
+                if (operationCounter > 999) {
+
+                    if (operationCounter > 99999) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return false;
+    }
+}
