6751d77a11d9ae7d64101eecf45458fc40021260
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-25 14:13:44 +0530

    File updated at 7/25/2026, 2:13:44 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,22 +1,17 @@
-// Last updated: 7/25/2026, 2:12:37 PM
+// Last updated: 7/25/2026, 2:13:44 PM
 1class Solution {
-2    public int largestRectangleArea(int[] heights) {
-3        Deque<Integer> stack = new ArrayDeque<>();
-4        int maxArea = 0;
-5        int n = heights.length;
-6
-7        for (int i = 0; i <= n; i++) {
-8            int currentHeight = (i == n) ? 0 : heights[i];
-9
-10            while (!stack.isEmpty() && currentHeight < heights[stack.peek()]) {
-11                int height = heights[stack.pop()];
-12                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
-13                maxArea = Math.max(maxArea, height * width);
-14            }
-15
-16            stack.push(i);
-17        }
-18
-19        return maxArea;
-20    }
-21}
+2    public int numDistinct(String s, String t) {
+3        int m = s.length();
+4        int n = t.length();
+5        int[] dp = new int[n + 1];
+6        dp[0] = 1;
+7        for (int i = 1; i <= m; i++) {
+8            for (int j = n; j >= 1; j--) {
+9                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+10                    dp[j] = dp[j] + dp[j - 1];
+11                }
+12            }
+13        }
+14        return dp[n];
+15    }
+16}
