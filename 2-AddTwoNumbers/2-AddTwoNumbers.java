81be5491388639c51387e41df4b4e86a1b074200
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-25 14:34:37 +0530

    File updated at 7/25/2026, 2:34:36 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,34 +1,24 @@
-// Last updated: 7/25/2026, 2:34:14 PM
-1class Solution {
-2    public int calculate(String s) {
-3        Deque<Integer> stack = new ArrayDeque<>();
-4        int currentNumber = 0;
-5        int result = 0;
-6        int sign = 1; 
-7        for (int i = 0; i < s.length(); i++) {
-8            char ch = s.charAt(i);
-9            if (Character.isDigit(ch)) {
-10                currentNumber = currentNumber * 10 + (ch - '0');
-11            } else if (ch == '+') {
-12                result += sign * currentNumber;
-13                currentNumber = 0;
-14                sign = 1;
-15            } else if (ch == '-') {
-16                result += sign * currentNumber;
-17                currentNumber = 0;
-18                sign = -1;
-19            } else if (ch == '(') {
-20                stack.push(result);
-21                stack.push(sign);
-22                result = 0;
-23                sign = 1;
-24            } else if (ch == ')') {
-25                result += sign * currentNumber;
-26                currentNumber = 0;
-27                result *= stack.pop(); 
-28                result += stack.pop(); 
-29            }
-30        }
-31        return result + (sign * currentNumber);
-32    }
-33}
+// Last updated: 7/25/2026, 2:34:36 PM
+class Solution {
+    int idx; // this index traverse the string in one pass, between different level of recursion
+    public int calculate(String s) {
+        idx = 0; // Initialization should be here
+        return calc(s);
+    }
+    
+    private int calc(String s) {
+        int res = 0, num = 0, sign = 1;
+        while (idx < s.length()) {
+            char c = s.charAt(idx++);
+            if (c >= '0' && c <= '9') num = num * 10 + c - '0';
+            else if (c == '(') num = calc(s); // ( is start of a new sub-problem, Let recursion solve the sub-problem
+            else if (c == ')') return res + sign * num;
+            else if (c == '+' || c == '-') { // only when we meet a new sign, we know a while number has been read
+                res += sign * num;
+                num = 0;
+                sign = c == '-' ? -1 : 1;
+            }
+        }
+        return res + sign * num; // last number is not processed yet
+    }
+}
