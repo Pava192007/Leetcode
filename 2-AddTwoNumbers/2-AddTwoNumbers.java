75fba874ac29fd008681c660cb40a9b4a94f9f74
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-25 14:23:05 +0530

    File updated at 7/25/2026, 2:23:05 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,24 +1,15 @@
-// Last updated: 7/25/2026, 2:22:25 PM
-1class Solution {
-2    public String shortestPalindrome(String s) {
-3        if (s == null || s.length() <= 1) {
-4            return s;
-5        }
-6        String reversed = new StringBuilder(s).reverse().toString();
-7        String temp = s + "#" + reversed;
-8        int[] lps = new int[temp.length()];
-9        for (int i = 1; i < temp.length(); i++) {
-10            int j = lps[i - 1];
-11            while (j > 0 && temp.charAt(i) != temp.charAt(j)) {
-12                j = lps[j - 1];
-13            }
-14            if (temp.charAt(i) == temp.charAt(j)) {
-15                j++;
-16            }
-17            lps[i] = j;
-18        }
-19        int longestPalindromePrefixLength = lps[temp.length() - 1];
-20        String suffixToAppend = reversed.substring(0, s.length() - longestPalindromePrefixLength);
-21        return suffixToAppend + s;
-22    }
-23}
+// Last updated: 7/25/2026, 2:23:05 PM
+class Solution {
+    public String shortestPalindrome(String s) {
+        int j = 0,n = s.length();
+        for(int i = n - 1; i >= 0; i--){
+            if(s.charAt(i) == s.charAt(j))
+                j++;
+        }
+        if(j == n)
+            return s;
+        String suf = s.substring(j);
+        String pre = new StringBuilder(suf).reverse().toString();
+        return pre + shortestPalindrome(s.substring(0, j)) + suf;
+    }
+}
