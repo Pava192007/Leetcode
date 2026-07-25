9197d503c41cdb3bdb76031d73ea66927d6829e2
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-25 14:24:26 +0530

    File updated at 7/25/2026, 2:24:26 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,15 +1,38 @@
-// Last updated: 7/25/2026, 2:23:05 PM
-class Solution {
-    public String shortestPalindrome(String s) {
-        int j = 0,n = s.length();
-        for(int i = n - 1; i >= 0; i--){
-            if(s.charAt(i) == s.charAt(j))
-                j++;
-        }
-        if(j == n)
-            return s;
-        String suf = s.substring(j);
-        String pre = new StringBuilder(suf).reverse().toString();
-        return pre + shortestPalindrome(s.substring(0, j)) + suf;
-    }
-}
+// Last updated: 7/25/2026, 2:24:26 PM
+1class Solution {
+2    public List<List<Integer>> getSkyline(int[][] buildings) {
+3        List<List<Integer>> result = new ArrayList<>();
+4        List<int[]> events = new ArrayList<>();
+5        for (int[] b : buildings) {
+6            events.add(new int[]{b[0], -b[2]});
+7            events.add(new int[]{b[1], b[2]});
+8        }
+9        Collections.sort(events, (a, b) -> {
+10            if (a[0] != b[0]) {
+11                return Integer.compare(a[0], b[0]);
+12            }
+13            return Integer.compare(a[1], b[1]);
+14        });
+15        PriorityQueue<Integer> maxHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+16        maxHeap.add(0); 
+17        int prevMaxHeight = 0;
+18        for (int[] event : events) {
+19            int x = event[0];
+20            int height = event[1];
+21            if (height < 0) {
+22                maxHeap.add(-height);
+23            } else {
+24                maxHeap.remove(height);
+25            }
+26            int currentMaxHeight = maxHeap.peek();
+27            if (currentMaxHeight != prevMaxHeight) {
+28                List<Integer> point = new ArrayList<>();
+29                point.add(x);
+30                point.add(currentMaxHeight);
+31                result.add(point);
+32                prevMaxHeight = currentMaxHeight;
+33            }
+34        }
+35        return result;
+36    }
+37}
