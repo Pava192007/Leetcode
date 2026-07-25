64507ca20dd95b88cb2aef1dc45c532fe16a024b
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-25 14:02:58 +0530

    File updated at 7/25/2026, 2:02:58 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,34 +1,17 @@
-// Last updated: 7/25/2026, 2:01:12 PM
-1/**
-2 * Definition for singly-linked list.
-3 * public class ListNode {
-4 *     int val;
-5 *     ListNode next;
-6 *     ListNode() {}
-7 *     ListNode(int val) { this.val = val; }
-8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
-9 * }
-10 */
-11class Solution {
-12    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-13        ListNode dummyHead = new ListNode(0);
-14        ListNode current = dummyHead;
-15        int carry = 0;
-16
-17        while (l1 != null || l2 != null || carry != 0) {
-18            int x = (l1 != null) ? l1.val : 0;
-19            int y = (l2 != null) ? l2.val : 0;
-20            
-21            int sum = x + y + carry;
-22            carry = sum / 10;
-23            
-24            current.next = new ListNode(sum % 10);
-25            current = current.next;
-26
-27            if (l1 != null) l1 = l1.next;
-28            if (l2 != null) l2 = l2.next;
-29        }
-30
-31        return dummyHead.next;
-32    }
-33}
+// Last updated: 7/25/2026, 2:02:58 PM
+1class Solution {
+2    public int lengthOfLongestSubstring(String s) {
+3        int maxLength = 0;
+4        int left = 0;
+5        Map<Character, Integer> map = new HashMap<>();
+6        for (int right = 0; right < s.length(); right++) {
+7            char currentChar = s.charAt(right);
+8            if (map.containsKey(currentChar)) {
+9                left = Math.max(left, map.get(currentChar) + 1);
+10            }
+11            map.put(currentChar, right);
+12            maxLength = Math.max(maxLength, right - left + 1);
+13        }
+14        return maxLength;
+15    }
+16}
