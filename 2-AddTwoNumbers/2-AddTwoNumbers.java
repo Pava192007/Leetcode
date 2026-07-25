00bb3095d6205924671801d5c07d8960d1d92bcb
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-25 14:16:33 +0530

    File updated at 7/25/2026, 2:16:33 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,37 +1,30 @@
-// Last updated: 7/25/2026, 2:15:15 PM
+// Last updated: 7/25/2026, 2:16:33 PM
 1class Solution {
-2    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-3        Set<String> wordSet = new HashSet<>(wordList);
-4        if (!wordSet.contains(endWord)) {
-5            return 0;
+2    public int maxPoints(int[][] points) {
+3        int n = points.length;
+4        if (n <= 2) {
+5            return n;
 6        }
-7        Queue<String> queue = new ArrayDeque<>();
-8        queue.add(beginWord);
-9        int level = 1;
-10        while (!queue.isEmpty()) {
-11            int size = queue.size();
-12            for (int i = 0; i < size; i++) {
-13                String currentWord = queue.poll();
-14                if (currentWord.equals(endWord)) {
-15                    return level;
-16                }
-17                char[] wordChars = currentWord.toCharArray();
-18                for (int j = 0; j < wordChars.length; j++) {
-19                    char originalChar = wordChars[j];
-20                    for (char c = 'a'; c <= 'z'; c++) {
-21                        if (wordChars[j] == c) continue;
-22                        wordChars[j] = c;
-23                        String newWord = new String(wordChars);
-24                        if (wordSet.contains(newWord)) {
-25                            queue.add(newWord);
-26                            wordSet.remove(newWord);
-27                        }
-28                    }
-29                    wordChars[j] = originalChar; 
-30                }
-31            }
-32            level++;
-33        }
-34        return 0;
-35    }
-36}
+7        int maxPoints = 1;
+8        for (int i = 0; i < n; i++) {
+9            Map<Double, Integer> slopeMap = new HashMap<>();
+10            for (int j = i + 1; j < n; j++) {
+11                int dx = points[j][0] - points[i][0];
+12                int dy = points[j][1] - points[i][1];
+13                double slope;
+14                if (dx == 0) {
+15                    slope = Double.POSITIVE_INFINITY;
+16                } else {
+17                    slope = (double) dy / dx;
+18                    if (slope == -0.0) {
+19                        slope = 0.0;
+20                    }
+21                }
+22                int count = slopeMap.getOrDefault(slope, 1) + 1;
+23                slopeMap.put(slope, count);
+24                maxPoints = Math.max(maxPoints, count);
+25            }
+26        }
+27        return maxPoints;
+28    }
+29}
