81907a8e934e2ad7f862350b3a18b3c71565db04
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-25 14:17:38 +0530

    File updated at 7/25/2026, 2:17:38 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,30 +1,28 @@
-// Last updated: 7/25/2026, 2:16:33 PM
+// Last updated: 7/25/2026, 2:17:38 PM
 1class Solution {
-2    public int maxPoints(int[][] points) {
-3        int n = points.length;
-4        if (n <= 2) {
-5            return n;
-6        }
-7        int maxPoints = 1;
-8        for (int i = 0; i < n; i++) {
-9            Map<Double, Integer> slopeMap = new HashMap<>();
-10            for (int j = i + 1; j < n; j++) {
-11                int dx = points[j][0] - points[i][0];
-12                int dy = points[j][1] - points[i][1];
-13                double slope;
-14                if (dx == 0) {
-15                    slope = Double.POSITIVE_INFINITY;
-16                } else {
-17                    slope = (double) dy / dx;
-18                    if (slope == -0.0) {
-19                        slope = 0.0;
-20                    }
-21                }
-22                int count = slopeMap.getOrDefault(slope, 1) + 1;
-23                slopeMap.put(slope, count);
-24                maxPoints = Math.max(maxPoints, count);
-25            }
-26        }
-27        return maxPoints;
-28    }
-29}
+2    public int maxProfit(int k, int[] prices) {
+3        if (prices == null || prices.length == 0 || k == 0) {
+4            return 0;
+5        }
+6        int n = prices.length;
+7        if (k >= n / 2) {
+8            int maxProfit = 0;
+9            for (int i = 1; i < n; i++) {
+10                if (prices[i] > prices[i - 1]) {
+11                    maxProfit += prices[i] - prices[i - 1];
+12                }
+13            }
+14            return maxProfit;
+15        }
+16        int[] buy = new int[k + 1];
+17        int[] sell = new int[k + 1];
+18        Arrays.fill(buy, Integer.MIN_VALUE);
+19        for (int price : prices) {
+20            for (int j = 1; j <= k; j++) {
+21                buy[j] = Math.max(buy[j], sell[j - 1] - price);
+22                sell[j] = Math.max(sell[j], buy[j] + price);
+23            }
+24        }
+25        return sell[k];
+26    }
+27}
