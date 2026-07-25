71e1ae040537b69fdbd58e211c54a90b1ef498c9
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-25 14:04:23 +0530

    File updated at 7/25/2026, 2:04:22 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,17 +1,33 @@
-// Last updated: 7/25/2026, 2:02:58 PM
+// Last updated: 7/25/2026, 2:04:22 PM
 1class Solution {
-2    public int lengthOfLongestSubstring(String s) {
-3        int maxLength = 0;
-4        int left = 0;
-5        Map<Character, Integer> map = new HashMap<>();
-6        for (int right = 0; right < s.length(); right++) {
-7            char currentChar = s.charAt(right);
-8            if (map.containsKey(currentChar)) {
-9                left = Math.max(left, map.get(currentChar) + 1);
-10            }
-11            map.put(currentChar, right);
-12            maxLength = Math.max(maxLength, right - left + 1);
-13        }
-14        return maxLength;
-15    }
-16}
+2    public List<List<Integer>> threeSum(int[] nums) {
+3        List<List<Integer>> result = new ArrayList<>();
+4        Arrays.sort(nums);
+5        for (int i = 0; i < nums.length - 2; i++) {
+6            if (i > 0 && nums[i] == nums[i - 1]) {
+7                continue;
+8            }
+9            int left = i + 1;
+10            int right = nums.length - 1;
+11            while (left < right) {
+12                int sum = nums[i] + nums[left] + nums[right];
+13                if (sum == 0) {
+14                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+15                    while (left < right && nums[left] == nums[left + 1]) {
+16                        left++;
+17                    }
+18                    while (left < right && nums[right] == nums[right - 1]) {
+19                        right--;
+20                    }
+21                    left++;
+22                    right--;
+23                } else if (sum < 0) {
+24                    left++; 
+25                } else {
+26                    right--; 
+27                }
+28            }
+29        }
+30        return result;
+31    }
+32}
