768b488b0f37a15c0e752ae0f28310eab723d2a1
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-25 14:22:26 +0530

    File updated at 7/25/2026, 2:22:25 PM

--- 2-AddTwoNumbers/2-AddTwoNumbers.java
+++ 2-AddTwoNumbers/2-AddTwoNumbers.java
@@ -1,28 +1,24 @@
-// Last updated: 7/25/2026, 2:17:38 PM
+// Last updated: 7/25/2026, 2:22:25 PM
 1class Solution {
-2    public int maxProfit(int k, int[] prices) {
-3        if (prices == null || prices.length == 0 || k == 0) {
-4            return 0;
+2    public String shortestPalindrome(String s) {
+3        if (s == null || s.length() <= 1) {
+4            return s;
 5        }
-6        int n = prices.length;
-7        if (k >= n / 2) {
-8            int maxProfit = 0;
-9            for (int i = 1; i < n; i++) {
-10                if (prices[i] > prices[i - 1]) {
-11                    maxProfit += prices[i] - prices[i - 1];
-12                }
+6        String reversed = new StringBuilder(s).reverse().toString();
+7        String temp = s + "#" + reversed;
+8        int[] lps = new int[temp.length()];
+9        for (int i = 1; i < temp.length(); i++) {
+10            int j = lps[i - 1];
+11            while (j > 0 && temp.charAt(i) != temp.charAt(j)) {
+12                j = lps[j - 1];
 13            }
-14            return maxProfit;
-15        }
-16        int[] buy = new int[k + 1];
-17        int[] sell = new int[k + 1];
-18        Arrays.fill(buy, Integer.MIN_VALUE);
-19        for (int price : prices) {
-20            for (int j = 1; j <= k; j++) {
-21                buy[j] = Math.max(buy[j], sell[j - 1] - price);
-22                sell[j] = Math.max(sell[j], buy[j] + price);
-23            }
-24        }
-25        return sell[k];
-26    }
-27}
+14            if (temp.charAt(i) == temp.charAt(j)) {
+15                j++;
+16            }
+17            lps[i] = j;
+18        }
+19        int longestPalindromePrefixLength = lps[temp.length() - 1];
+20        String suffixToAppend = reversed.substring(0, s.length() - longestPalindromePrefixLength);
+21        return suffixToAppend + s;
+22    }
+23}
