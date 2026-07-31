b6c448e4d44ec7d0eb9db82f580d7e1933478270
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-31 09:38:04 +0530

    File updated at 7/31/2026, 9:38:04 AM

--- 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
@@ -1,23 +1,41 @@
-// Last updated: 7/31/2026, 9:36:53 AM
-1class Solution {
-2    public int candy(int[] ratings) {
-3        int n = ratings.length;
-4        int[] candies = new int[n];
-5        Arrays.fill(candies, 1);
-6        for (int i = 1; i < n; i++) {
-7            if (ratings[i] > ratings[i - 1]) {
-8                candies[i] = candies[i - 1] + 1;
-9            }
-10        }
-11        for (int i = n - 2; i >= 0; i--) {
-12            if (ratings[i] > ratings[i + 1]) {
-13                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
-14            }
-15        }
-16        int totalCandies = 0;
-17        for (int count : candies) {
-18            totalCandies += count;
+// Last updated: 7/31/2026, 9:38:04 AM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public void reorderList(ListNode head) {
+13        if (head == null || head.next == null) return;
+14        ListNode slow = head;
+15        ListNode fast = head;
+16        while (fast != null && fast.next != null) {
+17            slow = slow.next;
+18            fast = fast.next.next;
 19        }
-20        return totalCandies;
-21    }
-22}
+20        ListNode prev = null;
+21        ListNode curr = slow.next;
+22        slow.next = null;
+23        while (curr != null) {
+24            ListNode nextTemp = curr.next;
+25            curr.next = prev;
+26            prev = curr;
+27            curr = nextTemp;
+28        }
+29        ListNode first = head;
+30        ListNode second = prev;
+31        while (second != null) {
+32            ListNode temp1 = first.next;
+33            ListNode temp2 = second.next;
+34            first.next = second;
+35            second.next = temp1;
+36            first = temp1;
+37            second = temp2;
+38        }
+39    }
+40}
