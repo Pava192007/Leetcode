f03a8802a5b8d96ce924aef5e834a299724782df
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-31 10:27:29 +0530

    File updated at 7/31/2026, 10:27:29 AM

--- 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
@@ -1,13 +1,12 @@
-// Last updated: 7/31/2026, 10:26:20 AM
+// Last updated: 7/31/2026, 10:27:29 AM
 1class Solution {
-2    public String convertToTitle(int columnNumber) {
-3        StringBuilder sb = new StringBuilder();
-4        while (columnNumber > 0) {
-5            columnNumber--; 
-6            char current = (char) ('A' + (columnNumber % 26));
-7            sb.append(current);
-8            columnNumber /= 26;
-9        }
-10        return sb.reverse().toString();
-11    }
-12}
+2    public int titleToNumber(String columnTitle) {
+3        int result = 0;
+4        for (int i = 0; i < columnTitle.length(); i++) {
+5            char c = columnTitle.charAt(i);
+6            int value = c - 'A' + 1;
+7            result = result * 26 + value;
+8        }
+9        return result;
+10    }
+11}
