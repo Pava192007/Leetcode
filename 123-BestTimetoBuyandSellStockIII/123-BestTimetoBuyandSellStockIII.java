cfc669fba14c47d8d85ea5b4a514e0deb21f723f
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-31 10:15:37 +0530

    File updated at 7/31/2026, 10:15:37 AM

--- 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
@@ -1,4 +1,4 @@
-// Last updated: 7/31/2026, 9:38:04 AM
+// Last updated: 7/31/2026, 10:15:37 AM
 1/**
 2 * Definition for singly-linked list.
 3 * public class ListNode {
@@ -10,32 +10,22 @@
 9 * }
 10 */
 11class Solution {
-12    public void reorderList(ListNode head) {
-13        if (head == null || head.next == null) return;
-14        ListNode slow = head;
-15        ListNode fast = head;
-16        while (fast != null && fast.next != null) {
-17            slow = slow.next;
-18            fast = fast.next.next;
-19        }
-20        ListNode prev = null;
-21        ListNode curr = slow.next;
-22        slow.next = null;
-23        while (curr != null) {
-24            ListNode nextTemp = curr.next;
-25            curr.next = prev;
-26            prev = curr;
-27            curr = nextTemp;
-28        }
-29        ListNode first = head;
-30        ListNode second = prev;
-31        while (second != null) {
-32            ListNode temp1 = first.next;
-33            ListNode temp2 = second.next;
-34            first.next = second;
-35            second.next = temp1;
-36            first = temp1;
-37            second = temp2;
-38        }
-39    }
-40}
+12    public ListNode insertionSortList(ListNode head) {
+13        if (head == null || head.next == null) {
+14            return head;
+15        }
+16        ListNode dummy = new ListNode(0);
+17        ListNode curr = head;
+18        while (curr != null) {
+19            ListNode nextTemp = curr.next;
+20            ListNode prev = dummy;
+21            while (prev.next != null && prev.next.val < curr.val) {
+22                prev = prev.next;
+23            }
+24            curr.next = prev.next;
+25            prev.next = curr;
+26            curr = nextTemp;
+27        }
+28        return dummy.next;
+29    }
+30}
