758e899e8573d1577b9476459bd34033390042f3
Pava192007 <Pavashinimurugesan@gmail.com>
2026-07-31 10:21:41 +0530

    File updated at 7/31/2026, 10:21:41 AM

--- 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
+++ 123-BestTimetoBuyandSellStockIII/123-BestTimetoBuyandSellStockIII.java
@@ -1,21 +1,16 @@
-// Last updated: 7/31/2026, 10:20:41 AM
+// Last updated: 7/31/2026, 10:21:41 AM
 1class Solution {
-2    public int maxProduct(int[] nums) {
-3        if (nums == null || nums.length == 0) return 0;
-4        int maxSoFar = nums[0];
-5        int minSoFar = nums[0];
-6        int result = nums[0];
-7        for (int i = 1; i < nums.length; i++) {
-8            int curr = nums[i];
-9            if (curr < 0) {
-10                int temp = maxSoFar;
-11                maxSoFar = minSoFar;
-12                minSoFar = temp;
-13            }
-14            maxSoFar = Math.max(curr, maxSoFar * curr);
-15            minSoFar = Math.min(curr, minSoFar * curr);
-16            result = Math.max(result, maxSoFar);
-17        }
-18        return result;
-19    }
-20}
+2    public int findMin(int[] nums) {
+3        int left = 0;
+4        int right = nums.length - 1;
+5        while (left < right) {
+6            int mid = left + (right - left) / 2;
+7            if (nums[mid] > nums[right]) {
+8                left = mid + 1;
+9            } else {
+10                right = mid;
+11            }
+12        }
+13        return nums[left];
+14    }
+15}
