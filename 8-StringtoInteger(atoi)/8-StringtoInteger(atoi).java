871dad300c182f0f5249604ab222afc1c55a112c
Pava192007 <Pavashinimurugesan@gmail.com>
2026-08-04 09:10:33 +0530

    File updated at 8/4/2026, 9:10:32 AM

--- 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
@@ -1,25 +1,16 @@
-// Last updated: 8/4/2026, 9:07:34 AM
+// Last updated: 8/4/2026, 9:10:32 AM
 1class Solution {
-2    public ListNode rotateRight(ListNode head, int k) {
-3        if (head == null || head.next == null || k == 0) {
-4            return head;
-5        }
-6        ListNode tail = head;
-7        int length = 1;
-8        while (tail.next != null) {
-9            tail = tail.next;
-10            length++;
-11        }
-12        tail.next = head;
-13        k = k % length;
-14        int stepsToNewTail = length - k;
-15        ListNode newTail = tail;
-16        while (stepsToNewTail > 0) {
-17            newTail = newTail.next;
-18            stepsToNewTail--;
-19        }
-20        ListNode newHead = newTail.next;
-21        newTail.next = null;
-22        return newHead;
-23    }
-24}
+2    public int[] plusOne(int[] digits) {
+3        int n = digits.length;
+4        for (int i = n - 1; i >= 0; i--) {
+5            if (digits[i] < 9) {
+6                digits[i]++;
+7                return digits;
+8            }
+9            digits[i] = 0;
+10        }
+11        int[] result = new int[n + 1];
+12        result[0] = 1; 
+13        return result;
+14    }
+15}
