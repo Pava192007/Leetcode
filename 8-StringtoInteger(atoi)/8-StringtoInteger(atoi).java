5bdf226528b38262816ed78707d7619a51425ba4
Pava192007 <Pavashinimurugesan@gmail.com>
2026-08-04 09:07:35 +0530

    File updated at 8/4/2026, 9:07:34 AM

--- 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
+++ 8-StringtoInteger(atoi)/8-StringtoInteger(atoi).java
@@ -1,18 +1,25 @@
-// Last updated: 8/4/2026, 9:01:19 AM
+// Last updated: 8/4/2026, 9:07:34 AM
 1class Solution {
-2    public ListNode removeNthFromEnd(ListNode head, int n) {
-3        ListNode dummy = new ListNode(0);
-4        dummy.next = head;
-5        ListNode fast = dummy;
-6        ListNode slow = dummy;
-7        for (int i = 0; i <= n; i++) {
-8            fast = fast.next;
-9        }
-10        while (fast != null) {
-11            fast = fast.next;
-12            slow = slow.next;
-13        }
-14        slow.next = slow.next.next;
-15        return dummy.next;
-16    }
-17}
+2    public ListNode rotateRight(ListNode head, int k) {
+3        if (head == null || head.next == null || k == 0) {
+4            return head;
+5        }
+6        ListNode tail = head;
+7        int length = 1;
+8        while (tail.next != null) {
+9            tail = tail.next;
+10            length++;
+11        }
+12        tail.next = head;
+13        k = k % length;
+14        int stepsToNewTail = length - k;
+15        ListNode newTail = tail;
+16        while (stepsToNewTail > 0) {
+17            newTail = newTail.next;
+18            stepsToNewTail--;
+19        }
+20        ListNode newHead = newTail.next;
+21        newTail.next = null;
+22        return newHead;
+23    }
+24}
