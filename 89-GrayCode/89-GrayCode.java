2e3b00da08e3a64df3ac2657050e3bb98889be25
Pava192007 <Pavashinimurugesan@gmail.com>
2026-09-22 09:20:02 +0530

    File updated at 9/22/2026, 9:20:02 AM

--- 89-GrayCode/89-GrayCode.java
+++ 89-GrayCode/89-GrayCode.java
@@ -1,20 +1,36 @@
-// Last updated: 9/22/2026, 9:11:49 AM
-1import java.util.ArrayList;
-2class Solution {
-3    public List<List<String>> groupAnagrams(String[] strs) {
-4        if (strs == null || strs.length == 0) {
-5            return new ArrayList<>();
-6        } 
-7        Map<String, List<String>> map = new HashMap<>();
-8        for (String str : strs) {
-9            char[] charArray = str.toCharArray();
-10            Arrays.sort(charArray);
-11            String sortedStr = String.valueOf(charArray);
-12            if (!map.containsKey(sortedStr)) {
-13                map.put(sortedStr, new ArrayList<>());
+// Last updated: 9/22/2026, 9:20:02 AM
+1class Solution {
+2    public List<Integer> spiralOrder(int[][] matrix) {
+3        List<Integer> result = new ArrayList<>();
+4        if (matrix == null || matrix.length == 0) {
+5            return result;
+6        }
+7        int top = 0;
+8        int bottom = matrix.length - 1;
+9        int left = 0;
+10        int right = matrix[0].length - 1;
+11        while (top <= bottom && left <= right) {
+12            for (int col = left; col <= right; col++) {
+13                result.add(matrix[top][col]);
 14            }
-15            map.get(sortedStr).add(str);
-16        }
-17        return new ArrayList<>(map.values());
-18    }
-19}
+15            top++;
+16            for (int row = top; row <= bottom; row++) {
+17                result.add(matrix[row][right]);
+18            }
+19            right--;
+20            if (top <= bottom) {
+21                for (int col = right; col >= left; col--) {
+22                    result.add(matrix[bottom][col]);
+23                }
+24                bottom--; 
+25            }
+26            if (left <= right) {
+27                for (int row = bottom; row >= top; row--) {
+28                    result.add(matrix[row][left]);
+29                }
+30                left++; 
+31            }
+32        }
+33        return result;
+34    }
+35}
